bc28852bb3224f54d5d2cc3dfb9dbe3ba88200be
Ricardo-USC <r_g108@student.usc.edu.au>
2024-05-16 14:47:33 +1000

    test api call

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,23 +1,13 @@
 <script>
-import { apiData, drinkNames } from './store.js';
+  let data;
 
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
-
-  const apiCall = ()=>
-    fetch("https://csc301-todoserver-plum.vercel.app/todos")
-  .then(response => response.json())
-  .then(data => {
-		console.log(data);
-    apiData.set(data);
-  }).catch(error => {
-    console.log(error);
-    return [];
-  });
+  async function apiCall() {
+		const response = await fetch('https://csc301-todoserver-plum.vercel.app/todos');
+		data = await response.json();
+	}
+  console.log(data);
 </script>
 
 <button on:click={apiCall}>
-  count is {count} yay
+  count is {0} yay
 </button>
